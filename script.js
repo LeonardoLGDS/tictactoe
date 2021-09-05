8bbddc9fb2f31a5ef8ffd4a7ad6ab2e5e8e02b2f
LeonardoLGDS <lgabriel.lgds@gmail.com>
2021-09-04 23:56:12 -0400

    exercise finished

--- script.js
+++ script.js
@@ -32,44 +32,80 @@ const Gameflux = (() => {
 
     cells.forEach((cell) => {
         cell.addEventListener('click', () => {
-            player1.playerChoice = cell.id;
-            let playerChoiceArrayElement = Number(player1.playerChoice[4]);
 
+            player1.playerChoice = Number(cell.id[4]);
 
-            if (Gameboard.boardArray.includes(playerChoiceArrayElement)) {
-                Gameboard.boardArray.splice(Gameboard.boardArray.indexOf(playerChoiceArrayElement), 1); // cells selected by the players are removed from the array of possible choices. 
-                player1.choicesHistory.push(playerChoiceArrayElement) // records the player selections.
+            if (Gameboard.boardArray.includes(player1.playerChoice)) {
+
+                // cells selected by the players are removed from the array of possible choices.
+                Gameboard.boardArray.splice(Gameboard.boardArray.indexOf(player1.playerChoice), 1);
+                // records the player selections. 
+                player1.choicesHistory.unshift(player1.playerChoice)
+
                 document.querySelector(`#${cell.id} > p`).innerHTML = "X";
-                player2.playerChoice = Gameboard.boardArray[Math.floor(Math.random() * Gameboard.boardArray.length)]; // Computer play. Its inside this IF to avoid a move by the computer, even when a player choose a illegal move i.e. click on an already filled cell.
+
+                // Its inside this IF to avoid an illegal move by the computer if the user clicks on an already filled cell.
+                computerPlay("easy");
             }
 
             if (Gameboard.boardArray.includes(player2.playerChoice)) {
-                Gameboard.boardArray.splice(Gameboard.boardArray.indexOf(player2.playerChoice), 1); // cells selected by the players are removed from the array of possible choices. 
-                player2.choicesHistory.push(player2.playerChoice) // records the player selections.
+
+                // cells selected by the players are removed from the array of possible choices. 
+                Gameboard.boardArray.splice(Gameboard.boardArray.indexOf(player2.playerChoice), 1);
+                // records the player selections.
+                player2.choicesHistory.unshift(player2.playerChoice);
+
                 document.querySelector(`#cell${player2.playerChoice} > p`).innerHTML = "O";
             }
 
-            // Checkes which player met the winningConditions first, then print the winner and reset the game.
-            for (let i = 0; i < Gameboard.winningConditions.length; i++) {
+            winnerCheck();
 
-                player1.winningConditionsMet = Gameboard.winningConditions[i].every((val) => player1.choicesHistory.includes(val));
-                if (player1.winningConditionsMet == true) {
-                    alert('Player1 is the winner!');
-                };
-                player2.winningConditionsMet = Gameboard.winningConditions[i].every((val) => player2.choicesHistory.includes(val));
-                if (player2.winningConditionsMet == true) {
-                    alert('Player2 is the winner!');
-                };
-                if (player1.winningConditionsMet == true || player2.winningConditionsMet == true || Gameboard.boardArray.length == 0) {
-                    if (Gameboard.boardArray.length == 0) { alert('Tie!') };
-                    player1.choicesHistory = [];
-                    player2.choicesHistory = [];
-                    Gameboard.boardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-                    cells.forEach((cell) => { document.querySelector(`#${cell.id} > p`).innerHTML = "" });
-                };
-            };
         });
     });
+    // Checks which player met the winningConditions first, then prints the winner and reset the game.
+    const winnerCheck = () => {
+
+        for (let i = 0; i < Gameboard.winningConditions.length; i++) {
+            player1.winningConditionsMet = Gameboard.winningConditions[i].every((elemt) => player1.choicesHistory.includes(elemt));
+            if (player1.winningConditionsMet == true) {
+                break;
+            };
+            player2.winningConditionsMet = Gameboard.winningConditions[i].every((elemt) => player2.choicesHistory.includes(elemt));
+            if (player2.winningConditionsMet == true) {
+                break;
+            };
+        };
+        if (player1.winningConditionsMet == true) {
+            alert('Player1 is the winner!');
+            resetGame(cells);
+        } else if (player2.winningConditionsMet == true) {
+            alert('Player2 is the winner!');
+            resetGame(cells);
+        } else if (Gameboard.boardArray.length == 0) {
+            alert('Tie!');
+            resetGame(cells);
+        };
+    };
+    const resetGame = (gridHtml) => {
+        player1.choicesHistory = [];
+        player2.choicesHistory = [];
+        Gameboard.boardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        gridHtml.forEach((cell) => { document.querySelector(`#${cell.id} > p`).innerHTML = "" });
+    };
+    const computerPlay = (level) => {
+        let counter = 0;
+        const check = (elemt) => {
+            counter += 1;
+            console.log(counter);
+            return player2.choicesHistory.includes(elemt);
+        };
+        if (level = "easy") {
+            player2.playerChoice = Gameboard.boardArray[Math.floor(Math.random() * Gameboard.boardArray.length)];
+        } else if (leve = "medium") {
+            player2.winningConditionsMet = Gameboard.winningConditions[i].every((elemt) => check(elemt));
+        } else {
+
+        };
+    };
     return { Gameboard, player1, player2 };
 })();
